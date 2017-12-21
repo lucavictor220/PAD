16ea2349461377a74c5e499d9f0e0cf6390000cf
Vitiok <lucavictor220@gmail.com>
2017-12-21 22:33:52 +0200

    Added class organization for Mediator.

--- lab2/Mediator.py
+++ lab2/Mediator.py
@@ -2,6 +2,45 @@ import socket
 import struct
 
 
+class Mediator:
+    def __init__(self, multicast_group, multicast_port):
+        # START multicast init
+        self.multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.multicast_group = multicast_group
+        self.multicast_port = multicast_port
+        self.multicast_socket.settimeout(0.7)
+        ttl = struct.pack('b', 1)
+        self.multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+        # END multicast init
+        # START unicast init
+        self.unicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        # END unicast init
+
+
+    def send_multicast_message(self):
+        message = "Hello world, work hard and don\'t give up"
+        try:
+            print("Send message: {}".format(message))
+            address = (self.multicast_group, self.multicast_port)
+            sent = self.multicast_socket.sendto(message.encode('utf-8'), address)
+            print("Message send!")
+            self.receive_message_unicast("127.0.0.1", 5005)
+        except socket.timeout:
+            print("Time out...")
+
+    def receive_message_unicast(self, ip, port):
+        self.unicast_socket.bind((ip, port))
+        while True:
+            print("waiting for message from nodes")
+            data, addr = self.unicast_socket.recvfrom(1024)  # buffer size is 1024 bytes
+            print("received message:", data.decode('utf-8'))
+
+
+mediator = Mediator('224.3.29.71', 10000)
+mediator.send_multicast_message()
+
+
+
 def receive_message_unicast(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     sock.bind((ip, port))
@@ -61,4 +100,4 @@ def send_tcp_message(ip, port):
     sock.send(message)
     sock.close()
 
-send_multicast_message("Hello world, work hard and don\'t give up", '224.3.29.71', 10000)
+# send_multicast_message("Hello world, work hard and don\'t give up", '224.3.29.71', 10000)
