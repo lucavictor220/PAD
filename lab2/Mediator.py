e53bfced4e71166e2293acdd1a49e8a2e4090d65
Vitiok <lucavictor220@gmail.com>
2017-12-21 20:55:48 +0200

    Created message to send and receive multicast message

--- lab2/Mediator.py
+++ lab2/Mediator.py
@@ -1,5 +1,5 @@
 import socket
-
+import struct
 
 def send_unicast_message(message, ip, port):
     print("UDP target IP:", ip)
@@ -8,4 +8,19 @@ def send_unicast_message(message, ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     sock.sendto(message.encode('utf-8'), (ip, port))
 
-send_unicast_message("hello world message", "127.0.0.1", 5005)
+
+def send_multicast_message(message, multicast_group, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(0.5)
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+    try:
+        print("Send message: {}".format(message))
+        address = (multicast_group, port)
+        sent = sock.sendto(message.encode('utf-8'), address)
+        print("Message send!")
+    except socket.timeout:
+        print("Time out...")
+
+
+send_multicast_message("Hello world, work hard and don\'t give up", '224.3.29.71', 10000)
--- lab2/Node.py
+++ lab2/Node.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 
 def receive_message_unicast(ip, port):
@@ -10,4 +11,20 @@ def receive_message_unicast(ip, port):
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         print("received message:", data.decode('utf-8'))
 
-receive_message_unicast("127.0.0.1", 5005)
+
+def receive_multicast_message(multicast_group, port):
+    server_address = ('', port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(server_address)
+    group = socket.inet_aton(multicast_group)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        print("Waiting for message...")
+        data, address = sock.recvfrom(1024)
+        print("Received message: {}".format(data.decode('utf-8')))
+        break
+
+
+receive_multicast_message('224.3.29.71', 10000)
