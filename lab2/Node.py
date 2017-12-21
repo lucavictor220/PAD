83e015af4fbcdabf63a8be4e5e2adf83d8454fe4
Vitiok <lucavictor220@gmail.com>
2017-12-21 21:01:53 +0200

    Fixed address in use issue

--- lab2/Node.py
+++ lab2/Node.py
@@ -11,19 +11,22 @@ def receive_message_unicast(ip, port):
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         print("received message:", data.decode('utf-8'))
 
-
 def receive_multicast_message(multicast_group, port):
-    server_address = ('', port)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(server_address)
-    group = socket.inet_aton(multicast_group)
-    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+
+    sock.bind((multicast_group, port))
+    host = socket.gethostbyname(socket.gethostname())
+    sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host))
+    sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP,
+                    socket.inet_aton(multicast_group) + socket.inet_aton(host))
 
     while True:
         print("Waiting for message...")
         data, address = sock.recvfrom(1024)
-        print("Received message: {}".format(data.decode('utf-8')))
+        print("Received message from {0}: {1}".format(address, data.decode('utf-8')))
         break
 
 
